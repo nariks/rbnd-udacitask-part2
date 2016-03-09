35cfe78c90e46db2deb80432abc198fde934df64
nariks <happycow@gmail.com>
2016-03-10 07:27:47 +1100

    Added format_date to listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,5 +2,27 @@ module Listable
   def format_description(description)
     "#{description}".ljust(25)
   end
+
+  def format_date (date1 = nil, date2 = nil)
+    if !date1 && !date2
+      date = "N/A"
+    elsif !date2
+      date = date1.strftime("%D")
+    else
+      date = date1.strftime("%D") + " -- " + date2.strftime("%D")
+    end
+    return date
+  end
+
+
+  #   date1 ? date1.strftime("%D") : "No due date"
+  # end
+
+  # def format_date
+  #   dates = @start_date.strftime("%D") if @start_date
+  #   dates << " -- " + @end_date.strftime("%D") if @end_date
+  #   dates = "N/A" if !dates
+  #   return dates
+  # end
   
 end
